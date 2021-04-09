e82e8af39fe686277ff219dc2607bba50e281f4b
Daniella Marks <marks.daniella@gmail.com>
2021-04-09 14:34:00 -0700

    css map page

--- map/map.js
+++ map/map.js
@@ -4,14 +4,24 @@ const section = document.querySelector('section');
 
 for (let quest of quests) {
     const anchorTag = document.createElement('a');
+    const div = document.createElement('div');
 
     anchorTag.textContent = quest.title;
 
     anchorTag.href = `../quest/?id=${quest.id}`;
-    section.append(anchorTag);
+
+    div.style.backgroundImage = `url(../assets/${quest.image})`;
+
+    div.append(anchorTag);
+    section.append(div);
 }
 
+// const imagesArray = [
 
+//     desert.jpg,
+
+
+// ]
 
 
 
